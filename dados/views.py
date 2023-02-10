7a0a14b22a10c329d4c862b5a7a99b0c05e21664
Andrimarciely Souza <andrimarciely@gmail.com>
2023-02-10 17:08:26 -0400

    criacao da view index.html

--- dados/views.py
+++ dados/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Gato
 # Create your views here.
+
+def index(request):
+    context = {
+        'Gato': Gato.objects.order_by('nome'),
+    }
+    return render(request,'index.html', context)
